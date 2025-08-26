0d9fd19a1a6a8c474f84846395b5190a36ff8c5a
someth chan <somethchan@gmail.com>
2025-08-26 20:28:08 +0000

    test2

--- qr_attendance_proj/http_listener.py
+++ qr_attendance_proj/http_listener.py
@@ -3,18 +3,17 @@ import ipaddress
 from fastapi import FastAPI, Request, HTTPException
 
 # Instantiate FastAPI object
-
 app = FastAPI(title="Listener API", version='0.1.1')
 
-# Define list of permitted IPs
+QR_SCRIPT = "/opt/jobs/qr_gen.py"
 
+# Define list of permitted IPs
 IP_ALLOWED = [ipaddress.ip_network(var) for var in [
     "44.3.2.28/32" # TAremote IP 1
     # "127.0.0.1/32" # TAremote IP 2
 ]]
 
 # Helper function to check if IP address is in the list of permitted IPs
-
 def check_ip(ip_str: str):
     ip = ipaddress.ip_address(ip_str)
     return any(ip in net for net in IP_ALLOWED)
@@ -25,6 +24,17 @@ def check_ip(ip_str: str):
 @app.middleware("http")
 async def enforce_allowlist(req: Request, call_next):
     client_ip = req.client.host
+    response = await call_next(req)
     if not check_ip(client_ip):
         raise HTTPException(status_code=403, detail="IP not permitted")
-    return await call_next(req)
+    return response
+
+# Read json data that has been posted to http://<server-ip>:<port>/data_qr
+@app.post("/data_qr")
+async def run_sript(req: Request)
+    data = await req.json()
+    
+    # Pass data from json to script
+
+    # Define the command to execute
+    cmd = [sys.executable, QR_SCRIPT]
